d611774c9259c49f83efd01e9e55a360ebe72713
Adrian Taylor <adetaylor@chromium.org>
2020-08-21 16:34:03 -0700

    Another note.

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -2,6 +2,9 @@ use autocxx_macro::include_cxx;
 
 // The following syntax is very interim as I haven't figured out
 // how to parse a string inside a procedural macro yet(!)
+// Also, 'input' is supposed to refer to input.h, but currently
+// that name is totally hardcoded in our hacky fork of bindgen, so don't
+// attempt to change it!
 include_cxx!(
     <input>,
     <DoMath>
