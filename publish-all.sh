f5f55cc274a59580a33a1026b0b79c689f0fe460
Adrian Taylor <adetaylor@chromium.org>
2020-09-11 17:05:31 -0700

    Publish script changes.

--- publish-all.sh
+++ publish-all.sh
@@ -7,15 +7,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DIRS="$DIR/engine $DIR $DIR/gen/build"
 
 for CRATE in $DIRS; do
+  pushd $CRATE
   echo "Dry run: $CRATE"
-  pushd $CRATE
   cargo publish --dry-run
-  popd
-done
-
-for CRATE in $DIRS; do
   echo "Publish: $CRATE"
-  pushd $CRATE
   cargo publish
   popd
 done
