13be8ba587e67186d0aa52746df372e4a1ef750b
Adrian Taylor <adetaylor@chromium.org>
2020-10-18 13:09:58 -0700

    Squash warnings.

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -375,6 +375,10 @@ impl IncludeCpp {
             items.push(syn::Item::Mod(itemmod));
         }
         let mut new_bindings: ItemMod = parse_quote! {
+            #[allow(non_snake_case)]
+            #[allow(dead_code)]
+            #[allow(non_upper_case_globals)]
+            #[allow(non_camel_case_types)]
             mod ffi {
             }
         };
