1bfd0fca7e281df23a77b02ad3af939b4964046f
Adrian Taylor <adetaylor@chromium.org>
2020-11-12 11:29:43 -0800

    Add test for returning reference.

--- engine/src/integration_tests.rs
+++ engine/src/integration_tests.rs
@@ -1892,6 +1892,28 @@ fn test_ns_func() {
     run_test(cxx, hdr, rs, &["C::give_bob"], &["A::B::Bob"]);
 }
 
+#[test]
+fn test_return_reference() {
+    let cxx = indoc! {"
+        const Bob& give_bob(const Bob& input_bob) {
+            return input_bob;
+        }
+    "};
+    let hdr = indoc! {"
+        #include <cstdint>
+        struct Bob {
+            uint32_t a;
+            uint32_t b;
+        };
+        const Bob& give_bob(const Bob& input_bob);
+    "};
+    let rs = quote! {
+        let b = ffi::Bob { a: 3, b: 4 };
+        assert_eq!(ffi::give_bob(&b).b, 4);
+    };
+    run_test(cxx, hdr, rs, &["give_bob"], &["Bob"]);
+}
+
 // Yet to test:
 // 1. Make UniquePtr<CxxStrings> in Rust
 // 3. Constants
