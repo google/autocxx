2d7937d940572b168b2d9a4dc0fd1a2af0a41ad9
Adrian Taylor <adetaylor@chromium.org>
2021-02-15 19:54:31 -0800

    Marking a TODO done.

--- engine/src/conversion/api.rs
+++ engine/src/conversion/api.rs
@@ -63,8 +63,8 @@ impl ConvertError {
     /// In the future we need to use this to provide diagnostics or logging to the user,
     /// which ideally we'd somehow winkle into the generated bindings
     /// in a way that causes them a compile-time problem only if they try to
-    /// _use_ the affects functions. I don't know a way to do that. Otherwise,
-    /// we should output these things as warnings during the codegen phase. TODO.
+    /// _use_ the affects functions. I don't know a way to do that. Meanwhile,
+    /// we should output these things as warnings during the codegen phase.
     pub(crate) fn is_ignorable(&self) -> bool {
         match self {
             ConvertError::VirtualThisType(..)
