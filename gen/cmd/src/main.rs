0b5391cc6792f4fe6b03e6eeae0460fe101ce2dd
Adrian Taylor <adetaylor@chromium.org>
2021-03-06 17:07:19 -0800

    Add cmd_test.

--- gen/cmd/src/main.rs
+++ gen/cmd/src/main.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(test)]
+mod cmd_test;
+
 use autocxx_engine::parse_file;
 use clap::{crate_authors, crate_version, App, Arg, SubCommand};
 use indoc::indoc;
