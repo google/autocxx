fa0dcf143aae6ab6bcec747c231204f82f66c602
Adrian Taylor <adetaylor@chromium.org>
2021-04-06 18:15:26 -0700

    Add s2 example

---
+++ examples/s2/README.md
@@ -0,0 +1,3 @@
+To build this example:
+* `git submodule update --init --recursive s2geometry`
+* `cargo run`
---
+++ examples/s2/build.rs
@@ -0,0 +1,26 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+fn main() {
+    // It's necessary to use an absolute path here because the
+    // C++ codegen and the macro codegen appears to be run from different
+    // working directories.
+    let path = std::path::PathBuf::from("s2geometry/src");
+    let path2 = std::path::PathBuf::from("src");
+    let defs: Vec<String> = Vec::new();
+    let mut b = autocxx_build::build("src/main.rs", &[&path, &path2], &defs).unwrap();
+    b.flag_if_supported("-std=c++14")
+        .compile("autocxx-s2-example");
+    println!("cargo:rerun-if-changed=src/main.rs");
+}
