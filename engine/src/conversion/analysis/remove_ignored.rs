155737a81f8f2241aca4bfeb64152eb0f8aa534d
Adrian Taylor <adetaylor@chromium.org>
2021-04-29 13:27:54 -0700

    Ignore unknown types

--- engine/src/conversion/analysis/remove_ignored.rs
+++ engine/src/conversion/analysis/remove_ignored.rs
@@ -15,48 +15,64 @@
 use std::collections::HashSet;
 
 use super::fun::FnAnalysis;
-use crate::conversion::api::{Api, ApiDetail};
-use crate::conversion::convert_error::ErrorContext;
+use crate::conversion::{convert_error::ErrorContext, ConvertError};
+use crate::{
+    conversion::api::{Api, ApiDetail},
+    known_types,
+};
 
 /// Remove any APIs which depend on other items which have been ignored.
 pub(crate) fn filter_apis_by_ignored_dependents(
     mut apis: Vec<Api<FnAnalysis>>,
 ) -> Vec<Api<FnAnalysis>> {
-    let mut ignored_items: HashSet<_> = apis
-        .iter()
-        .filter_map(|api| {
-            if matches!(
-                api.detail,
-                ApiDetail::IgnoredItem {
-                    ctx: ErrorContext::Item(..),
-                    ..
-                }
-            ) {
-                Some(api.typename())
-            } else {
-                None
+    let (ignored_items, valid_items): (Vec<&Api<_>>, Vec<&Api<_>>) = apis.iter().partition(|api| {
+        matches!(
+            api.detail,
+            ApiDetail::IgnoredItem {
+                ctx: ErrorContext::Item(..),
+                ..
             }
-        })
+        )
+    });
+    let mut ignored_items: HashSet<_> = ignored_items
+        .into_iter()
+        .map(|api| api.typename())
         .collect();
+    let valid_types: HashSet<_> = valid_items.into_iter().map(|api| api.typename()).collect();
     let mut iterate_again = true;
     while iterate_again {
         iterate_again = false;
         apis = apis
             .into_iter()
-            .filter(|api| {
+            .map(|api| {
                 if api.deps.iter().any(|dep| ignored_items.contains(dep)) {
                     iterate_again = true;
                     ignored_items.insert(api.typename());
-                    eprintln!(
-                        "Skipping item {} because it depends on another item we skipped.",
-                        api.typename().to_string()
-                    );
-                    false
+                    create_ignore_item(api, ConvertError::IgnoredDependent)
+                } else if !api
+                    .deps
+                    .iter()
+                    .all(|dep| valid_types.contains(dep) || known_types().is_known_type(dep))
+                {
+                    iterate_again = true;
+                    ignored_items.insert(api.typename());
+                    create_ignore_item(api, ConvertError::UnknownDependentType)
                 } else {
-                    true
+                    api
                 }
             })
             .collect();
     }
     apis
+}
+
+fn create_ignore_item(api: Api<FnAnalysis>, err: ConvertError) -> Api<FnAnalysis> {
+    Api {
+        name: api.typename(),
+        deps: HashSet::new(),
+        detail: ApiDetail::IgnoredItem {
+            err,
+            ctx: ErrorContext::Item(api.typename().get_final_ident()), // TODO
+        },
+    }
 }
