fc2c9ffa5d4a8e2e14f6fd483bc0aa32776155ba
Adrian Taylor <adetaylor@chromium.org>
2021-05-16 09:19:36 -0700

    Another fix

--- tools/reduce/src/main.rs
+++ tools/reduce/src/main.rs
@@ -203,7 +203,7 @@ fn do_run(matches: ArgMatches, tmp_dir: &TempDir) -> Result<(), std::io::Error> 
         .to_string();
     let gen_cmd = matches.value_of("gen-cmd").unwrap_or(&default_gen_cmd);
     run_sample_gen_cmd(gen_cmd, &rs_path, &tmp_dir.path(), &extra_clang_args)?;
-    let clang = PathBuf::from(matches.value_of("clang-path").unwrap_or("clang++"));
+    let clang = PathBuf::from(matches.value_of("clang").unwrap_or("clang++"));
     let interestingness_test = tmp_dir.path().join("test.sh");
     let precompile_step = if matches.is_present("compile") {
         Some(concat_path.to_str().unwrap())
