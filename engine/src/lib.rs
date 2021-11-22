221ac7491c96ea3193d3339014b9e365b56c3a65
Adrian Taylor <adetaylor@chromium.org>
2021-11-21 16:18:29 -0800

    Document Builder objects.
    Fixes #677.

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -16,6 +16,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![feature(doc_cfg)]
+
 mod ast_discoverer;
 mod conversion;
 mod cxxbridge;
