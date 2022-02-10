36cef8428c72ec5daa2de15d78d1b837d038071c
Adrian Taylor <adetaylor@chromium.org>
2022-02-09 19:06:09 -0800

    Always RenameInOutputMod instead of #[rust_name]
    Previously we generated slightly simpler output by using
    `#[rust_name]` attributes on the cxx::bridge mod. With this change,
    we never do that and instead always perform any such renames
    with a `use` statement in the output set of mods.
    
    This simplifies our code, while making output more verbose;
    it is a prerequisite for a subsequent step towards solving #486.

--- engine/src/conversion/codegen_rs/fun_codegen.rs
+++ engine/src/conversion/codegen_rs/fun_codegen.rs
@@ -80,14 +80,6 @@ pub(super) fn gen_function(
     let mut cpp_name_attr = Vec::new();
     let mut impl_entry = None;
     let mut trait_impl_entry = None;
-    let rust_name_attr: Vec<_> = match &analysis.rust_rename_strategy {
-        RustRenameStrategy::RenameUsingRustAttr => Attribute::parse_outer
-            .parse2(quote!(
-                #[rust_name = #rust_name]
-            ))
-            .unwrap(),
-        _ => Vec::new(),
-    };
     let wrapper_unsafety = analysis.requires_unsafe.wrapper_token();
     let fn_generator = FnGenerator {
         param_details: &param_details,
@@ -176,7 +168,6 @@ pub(super) fn gen_function(
     let bridge_unsafety = analysis.requires_unsafe.bridge_token();
     let extern_c_mod_item = ForeignItem::Fn(parse_quote!(
         #(#namespace_attr)*
-        #(#rust_name_attr)*
         #(#cpp_name_attr)*
         #doc_attr
         #vis #bridge_unsafety fn #cxxbridge_name #lifetime_tokens ( #params ) #ret_type;
