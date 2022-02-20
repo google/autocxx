9a9ef95093f64e0eba6af2c748c13ce68c464a00
Philip Craig <689193+philipcraig@users.noreply.github.com>
2022-02-20 16:42:22 +0000

    Make reduce_test an integration test
    reduce_test should be an integration test, as it depends
    upon its binaries to run tests
    
    This change lets `cargo test` work without a prior `cargo build`

--- tools/reduce/src/main.rs
+++ tools/reduce/src/main.rs
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[cfg(test)]
-mod reduce_test;
-
 use std::{
     fs::File,
     io::Write,
