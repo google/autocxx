6fd8d4f5b79d854bc14686d41c6520208efeb91e
Brian Silverman <bsilver16384@gmail.com>
2022-03-02 22:34:58 -0800

    Test that code generated with `gen` compiles

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -21,7 +21,9 @@ use std::{
     sync::Mutex,
 };
 
-use autocxx_engine::{Builder, BuilderContext, BuilderError, RebuildDependencyRecorder, HEADER};
+use autocxx_engine::{
+    Builder, BuilderBuild, BuilderContext, BuilderError, RebuildDependencyRecorder, HEADER,
+};
 use log::info;
 use once_cell::sync::OnceCell;
 use proc_macro2::{Span, TokenStream};
@@ -54,6 +56,50 @@ pub fn doctest(
     r
 }
 
+fn configure_builder(b: &mut BuilderBuild) -> &mut BuilderBuild {
+    let target = rust_info::get().target_triple.unwrap();
+    b.host(&target)
+        .target(&target)
+        .opt_level(1)
+        .flag("-std=c++14") // For clang
+        .flag_if_supported("/GX") // Enable C++ exceptions for msvc
+}
+
+/// API to test building pre-generated files.
+pub fn build_from_folder(
+    folder: &Path,
+    main_rs_file: &Path,
+    generated_rs_files: Vec<PathBuf>,
+    cpp_files: &[&str],
+) -> Result<(), TestError> {
+    let target_dir = folder.join("target");
+    std::fs::create_dir(&target_dir).unwrap();
+    let mut b = BuilderBuild::new();
+    for cpp_file in cpp_files.iter() {
+        b.file(folder.join(cpp_file));
+    }
+    configure_builder(&mut b)
+        .out_dir(&target_dir)
+        .include(folder)
+        .include(folder.join("demo"))
+        .try_compile("autocxx-demo")
+        .map_err(TestError::CppBuild)?;
+    // use the trybuild crate to build the Rust file.
+    let r = get_builder().lock().unwrap().build(
+        &target_dir,
+        "autocxx-demo",
+        &folder,
+        &["input.h", "cxx.h"],
+        &main_rs_file,
+        generated_rs_files,
+    );
+    if r.is_err() {
+        return Err(TestError::RsBuild); // details of Rust panic are a bit messy to include, and
+                                        // not important at the moment.
+    }
+    Ok(())
+}
+
 fn get_builder() -> &'static Mutex<LinkableTryBuilder> {
     static INSTANCE: OnceCell<Mutex<LinkableTryBuilder>> = OnceCell::new();
     INSTANCE.get_or_init(|| Mutex::new(LinkableTryBuilder::new()))
@@ -77,7 +123,7 @@ impl LinkableTryBuilder {
     }
 
     fn move_items_into_temp_dir<P1: AsRef<Path>>(&self, src_path: &P1, pattern: &str) {
-        for item in std::fs::read_dir(src_path).unwrap() {
+        for item in std::fs::read_dir(src_path).expect(&src_path.as_ref().to_string_lossy()) {
             let item = item.unwrap();
             if item.file_name().into_string().unwrap().contains(pattern) {
                 let dest = self.temp_dir.path().join(item.file_name());
@@ -387,8 +433,6 @@ pub fn do_run_test_manual(
         }
     }
 
-    let target = rust_info::get().target_triple.unwrap();
-
     if !cxx_code.is_empty() {
         // Step 4: Write the C++ code snippet to a .cc file, along with a #include
         //         of the header emitted in step 5.
@@ -397,13 +441,7 @@ pub fn do_run_test_manual(
         b.file(cxx_path);
     }
 
-    let b = b
-        .out_dir(&target_dir)
-        .host(&target)
-        .target(&target)
-        .opt_level(1)
-        .flag("-std=c++14") // For clang
-        .flag_if_supported("/GX"); // Enable C++ exceptions for msvc
+    let b = configure_builder(&mut b).out_dir(&target_dir);
     let b = if let Some(builder_modifier) = builder_modifier {
         builder_modifier.modify_cc_builder(b)
     } else {
