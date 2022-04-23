b304386daf045dc6d6a1827370cf73d7f9a2de9c
Adrian Taylor <adetaylor@chromium.org>
2022-04-22 17:58:25 -0700

    Adjustment.

--- engine/src/conversion/codegen_cpp/mod.rs
+++ engine/src/conversion/codegen_cpp/mod.rs
@@ -605,7 +605,7 @@ impl<'a> CppCodeGenerator<'a> {
         };
         let mut headers = vec![Header::System("memory")];
         if need_allocators {
-            headers.push(Header::System("cstddef"));
+            headers.push(Header::System("stddef.h"));
             headers.push(Header::NewDeletePrelude);
         }
         Ok(ExtraCpp {
