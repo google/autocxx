ca6aef932d908e28f66de7521b2fcadf8620ddf8
Adrian Taylor <adetaylor@chromium.org>
2022-04-27 09:35:56 -0700

    Add test for 'array' is not a class, namespace, or enumeration

--- integration-tests/tests/integration_test.rs
+++ integration-tests/tests/integration_test.rs
@@ -11006,6 +11006,40 @@ fn test_typedef_unsupported_type_pri() {
 }
 
 #[test]
+fn test_array_trouble1() {
+    let hdr = indoc! {"
+        namespace a {
+        template <typename b> struct array {
+          typedef b c;
+          typedef c d;
+        };
+        } // namespace a
+    "};
+    run_test_ex(
+        "",
+        hdr,
+        quote! {},
+        quote! { generate_all!() },
+        None,
+        None,
+        None,
+    );
+}
+
+#[test]
+fn test_array_trouble2() {
+    let hdr = indoc! {"
+        namespace a {
+        template <typename b> struct array {
+          typedef b c;
+          typedef c d;
+        };
+        } // namespace a
+    "};
+    run_test("", hdr, quote! {}, &["a::array_d"], &[]);
+}
+
+#[test]
 fn test_pass_rust_str_and_return_struct() {
     let cxx = indoc! {"
         A take_str_return_struct(rust::Str) {
