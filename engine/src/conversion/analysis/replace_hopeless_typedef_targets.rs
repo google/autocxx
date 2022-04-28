d5912d4fff547465f7e5c58d836ddcb8f8338153
Adrian Taylor <adetaylor@chromium.org>
2022-04-27 19:07:38 -0700

    Ignore inner types which are opaque typedefs.
    Fixes #1077.

--- engine/src/conversion/analysis/replace_hopeless_typedef_targets.rs
+++ engine/src/conversion/analysis/replace_hopeless_typedef_targets.rs
@@ -10,8 +10,11 @@ use indexmap::set::IndexSet as HashSet;
 
 use crate::{
     conversion::{
-        analysis::tdef::TypedefAnalysis, api::Api, apivec::ApiVec,
-        convert_error::ConvertErrorWithContext,
+        analysis::tdef::TypedefAnalysis,
+        api::Api,
+        apivec::ApiVec,
+        convert_error::{ConvertErrorWithContext, ErrorContext},
+        ConvertError,
     },
     types::QualifiedName,
 };
@@ -44,12 +47,36 @@ pub(crate) fn replace_hopeless_typedef_targets(apis: ApiVec<PodPhase>) -> ApiVec
     apis.into_iter()
         .map(|api| match api {
             Api::Typedef {
+                ref name,
                 analysis: TypedefAnalysis { ref deps, .. },
                 ..
-            } if !ignored_types.is_disjoint(deps) => Api::OpaqueTypedef {
-                name: api.name_info().clone(),
-                forward_declaration: false,
-            },
+            } if !ignored_types.is_disjoint(deps) =>
+            // This typedef depended on something we ignored.
+            // Ideally, we'd turn it into an opaque item.
+            // We can't do that if this is an inner type,
+            // because we have no way to know if it's abstract or not,
+            // and we can't represent inner types in cxx without knowing
+            // that.
+            {
+                let name_id = name.name.get_final_ident();
+                if api
+                    .cpp_name()
+                    .as_ref()
+                    .map(|n| n.contains("::"))
+                    .unwrap_or_default()
+                {
+                    Api::IgnoredItem {
+                        name: api.name_info().clone(),
+                        err: ConvertError::NestedOpaqueTypedef,
+                        ctx: Some(ErrorContext::new_for_item(name_id)),
+                    }
+                } else {
+                    Api::OpaqueTypedef {
+                        name: api.name_info().clone(),
+                        forward_declaration: false,
+                    }
+                }
+            }
             Api::Typedef {
                 analysis: TypedefAnalysis { ref deps, .. },
                 ..
