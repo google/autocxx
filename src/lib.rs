ebf8582663f58d31d40637192a8fb188cc3462c1
Adrian Taylor <adetaylor@chromium.org>
2023-06-14 14:20:05 +0100

    Improve reference wrapper example.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![doc = include_str!("../README.md")]
 
+#![feature(dispatch_from_dyn)]
+#![feature(unsize)]
+
 // Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
