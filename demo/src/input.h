da0d5bc23666917be6395abd9f141804db65c7d3
Adrian Taylor <adetaylor@chromium.org>
2025-02-26 22:16:37 +0000

    Add some doc comments explaining mods encountered.
    This helps when following our documentation's advice which is to use cargo doc
    --document-private-items.

--- demo/src/input.h
+++ demo/src/input.h
@@ -13,15 +13,20 @@
 #include <stdint.h>
 #include <string>
 
+/// A C++ goat.
 class Goat {
 public:
+    /// Goat constructor.
     Goat() : horns(0) {}
+    /// Add a horn to the goat.
     void add_a_horn();
+    /// Describe the goat.
     std::string describe() const;
 private:
     uint32_t horns;
 };
 
+/// Do some maths!
 inline uint32_t DoMath(uint32_t a) {
     return a * 3;
 }
